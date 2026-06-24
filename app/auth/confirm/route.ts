503123dbd953fe1ef272a858c4fc129b5cdc49d1
marcelotuemura <marcelotuemura@gmail.com>
2026-06-24 15:16:09 -0400

    Update route.ts

--- app/auth/confirm/route.ts
+++ app/auth/confirm/route.ts
@@ -1,6 +1,6 @@
 import { type EmailOtpType } from '@supabase/supabase-js'
 import { NextRequest, NextResponse } from 'next/server'
-import { createServerSupabaseClien } from '@/lib/supabase-server'
+import { createServerSupabaseClient } from '@/lib/supabase-server'
 
 export async function GET(request: NextRequest) {
   const { searchParams, origin } = new URL(request.url)
