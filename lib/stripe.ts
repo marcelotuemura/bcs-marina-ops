3e13c0b1bc2ec3a152c3939dbc3762dcb93fb4f8
marcelotuemura <marcelotuemura@gmail.com>
2026-06-26 21:29:31 -0400

    Delete lib directory

--- lib/stripe.ts
+++
@@ -1,7 +0,0 @@
-import Stripe from 'stripe'
-
-export function getStripe() {
-  const key = process.env.STRIPE_SECRET_KEY
-  if (!key) throw new Error('Missing STRIPE_SECRET_KEY in .env.local')
-  return new Stripe(key)
-}
